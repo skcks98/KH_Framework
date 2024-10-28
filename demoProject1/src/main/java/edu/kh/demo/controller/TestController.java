5d4b14aae80758cb69d05cbdf0af8bffc3ea8a19
skcks98 <skcks98@gmail.com>
2024-10-28 16:45:22 +0900

    Spring Boot 공부

--- demoProject1/src/main/java/edu/kh/demo/controller/TestController.java
+++ demoProject1/src/main/java/edu/kh/demo/controller/TestController.java
@@ -12,79 +12,70 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller // 요청/응답을 제어할 컨트롤러 역할임을 명시
 			// + Bean 등록
 public class TestController {
+	
 	// 기존 Servlet : 클래스 단위로 하나의 요청만 처리 가능
 	// Spring : 메서드 단위로 요청 처리 가능
 	
-	//@RequestMapping("요청주소")
+	
+	// @RequestMapping("요청주소")
 	// - 요청주소를 처리할 메서드를 매핑하는 어노테이션
 	
-	// 1) 메서드에 작성 :
+	// 1) 메서드에 작성 : 
 	// - 요청 주소와 해당 메서드를 매핑
-	// GET/POST 가리지 않고 매핑 (속성을 통해서 지정 가능 or 다른 어노테이션을 이용해서 가능)
+	// - GET/POST 가리지 않고 매핑 (속성을 통해서 지정 가능 or 다른 어노테이션을 이용해서 가능)
 	
 	/*
-	@RequestMapping(value = "/hello", method=RequestMethod.POST)
+	@RequestMapping(value="/hello", method=RequestMethod.POST)
 	public String hello() {
 		// 메서드 로직
 		
-		
-		return "경로...";
-	}*/
+		return "경로..";
+	}
+	*/
 	
-	
-	
-	/*
 	// 2) 클래스에 작성
 	// - 공통 주소를 매핑
-	// ex) todo/insert,todo/select. todo/update..
+	// ex) /todo/insert,  /todo/select,  /todo/update ..
 	
-	*/
 	/*
-	public class 클래스명 {
-	@RequestMapping("/insert")
-	public String 메서드명 {}
+	@RequestMapping("/todo")
+	 public class 클래스명 {
 	
-	@RequestMapping("/select")
+		@RequestMapping("/insert") // /todo/insert 매핑
+		public String 메서드명() {}
+		
+		@RequestMapping("/select") // /todo/select 매핑
+		public String 메서드명() {}
+		
+		@RequestMapping("/update") // /todo/update 매핑
+		public String 메서드명() {}
 	
-	@RequestMapping("/update")
+	}
 	*/
 	
-	
+	@RequestMapping("/test") // /test 요청 시 처리할 메서드 매핑
+	public String testMethod() {
+		System.out.println("/test 요청 받음");
+		
 		/*
-		 * Controller 메서드으 ㅣ반환형이 String인 경우
-		 * - 메서드에서 반환되는 문자열이 forwdord
+		 * Controller 메서드의 반환형이 String인 이유
+		 * - 메서드에서 반환되는 문자열이
+		 *   forward 할 html 파일의 경로가 되기 때문!
 		 * 
 		 * 
-		 * thymeleaf : JSP 대신 사용하는 템플릿 엔진(html 형태)
+		 * Thymeleaf : JSP 대신 사용하는 템플릿 엔진(html 형태)
 		 * 
-		 * classpatr : /templates
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
+		 * classpath: == src/main/resources
+		 * 접두사 : classpath:/templates/
+		 * 접미사 : .html 
 		 * 
 		 * */
 		
+		// src/main/resources/templates/test.html
+		return "test"; // 접두사 + 반환값 + 접미사 경로의 html로 forward
 		
-		
-		// src/main/resources/templates/test,xml;
-		
-	
-	
-	
-	
-	
-	
-}
-	
-	
-	
-	
-	
-	
-	
-	
+		/* 접두사, 접미사, forward 설정은 View Resolver 객체가 담당 */
+	}
 	
 	
 	
@@ -98,3 +89,4 @@ public class TestController {
 	
 	
 
+}
