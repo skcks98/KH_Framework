27c42fd6dbc66caca229109f338da3968942fb88
skcks98 <skcks98@gmail.com>
2024-11-13 17:51:02 +0900

    commit

--- boardProject/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,7 +1,9 @@
 package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -13,5 +15,15 @@ public class MainController {
 		// classpath://templates/
 		// .html
 		return "common/main";
+	}
+	
+	
+	// LoginFilter -> loginError 리다이렉트
+	// -> message만들어서 메인페이지로 리다이렉트
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		ra.addFlashAttribute("message","로그인 후 이용해주세요");
+		
+		return "redirect:/";
 	}
 }
