e0c56b0ed3d9bd1e0e6604f8c3c8df3665a93528
skcks98 <skcks98@gmail.com>
2025-01-15 16:34:30 +0900

    commit

--- boardProject/src/test/java/edu/kh/todo/BoardProjectApplicationTests.java
+++ boardProject/src/test/java/edu/kh/todo/BoardProjectApplicationTests.java
@@ -3,11 +3,18 @@ package edu.kh.todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import edu.kh.project.BoardProjectApplication;
+
+@SpringBootTest(classes = BoardProjectApplication.class)
+
+// Spring Boot 테스트를 실행하기 위한 어노테이션
+// 스프링 애플리케이션 컨텍스트를 로드하고, 애플리케이션의 전체 환경을 테스트 할 수 있게 해줌
 class BoardProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("애플리케이션 컨텍스트 로드 완료.");
 	}
+
 
 }
